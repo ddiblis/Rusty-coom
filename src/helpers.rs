e5f7d5133b4e71afaaf0c2d75d91a3f90f8c8641
“sdl <sdltesterusa131@gmail.com>
2021-12-24 23:17:38 -0700

    feat(main.rs-helpers.rs): Download function, file formatting functionality

--- src/helpers.rs
+++ src/helpers.rs
@@ -1,93 +1,104 @@
-use bytes::Buf;
 use std::io::Cursor;
+use bytes::Buf;
 
 use http::{header::COOKIE, HeaderMap, HeaderValue};
-use select::document::Document;
+use image::io::Reader as ImageReader;
 use select::predicate::{Class, Name};
+use select::document::Document;
+use image::ImageFormat;
 
 async fn get_dom(url: &str) -> Result<Document, Box<dyn std::error::Error>> {
-  let mut headers = HeaderMap::new();
-  headers.append(
-      COOKIE,
-      HeaderValue::from_str("__ddg2=6fryH34fRixR8HCV").unwrap(),
-  );
+    let mut headers = HeaderMap::new();
+    headers.append(COOKIE, HeaderValue::from_str("__ddg2=6fryH34fRixR8HCV")?);
 
-  println!("{}", url);
-  let client = reqwest::Client::new();
-  let resp = client.get(url).headers(headers).send().await?;
+    println!("{}", url);
+    let client = reqwest::Client::new();
+    let resp = client.get(url).headers(headers).send().await?;
 
-  assert!(resp.status().is_success());
+    assert!(resp.status().is_success());
 
-  let body = Cursor::new(resp.bytes().await?).reader();
-  let document = Document::from_read(body).unwrap();
-  Ok(document)
+    let body = Cursor::new(resp.bytes().await?).reader();
+    let document = Document::from_read(body)?;
+    Ok(document)
 }
 
 pub async fn gen_links(
-  url: &str,
-  class_name: &str,
-  page_num: usize,
-  base_url: &str,
+    url: &str,
+    class_name: &str,
+    page_num: usize,
+    base_url: &str,
 ) -> Result<Vec<String>, Box<dyn std::error::Error>> {
-  let doc = get_dom(&format!("{}?o={:?}", &url, page_num * 25))
-      .await
-      .unwrap();
-  let mut links = Vec::new();
-  for node in doc.find(Class(class_name)) {
-      let name = node.find(Class("fancy-link")).next();
-      match name {
-          Some(value) => links.push([base_url, value.attr("href").unwrap()].join("")),
-          None => {}
-      }
-  }
-  Ok(links)
+    let doc = get_dom(&format!("{}?o={:?}", &url, page_num * 25)).await?;
+    let mut links = Vec::new();
+    for node in doc.find(Class(class_name)) {
+        let name = node.find(Class("fancy-link")).next();
+        match name {
+            Some(value) => links.push([base_url, value.attr("href").unwrap()].join("")),
+            None => {}
+        }
+    }
+    Ok(links)
 }
 
 pub async fn get_page_num(link: &str) -> Result<usize, Box<dyn std::error::Error>> {
-  let mut page_len = 0;
-  let doc = get_dom(&link).await.unwrap();
+    let mut page_len = 0;
+    let doc = get_dom(&link).await.unwrap();
 
-  for node in doc.find(Class("paginator")).next() {
-      let small_node = node.find(Name("small")).next().unwrap().text();
-      let split = small_node.split(" ").last().unwrap();
-      let num: usize = split[0..split.len() - 1].parse().unwrap();
-      page_len = match num {
-          n if n % 25 == 0 => num / 25,
-          n if n % 25 != 0 => num / 25 + 1,
-          _ => 0,
-      }
-  }
-  Ok(page_len)
+    for node in doc.find(Class("paginator")).next() {
+        let small_node = node.find(Name("small")).next().unwrap().text();
+        let split = small_node.split(" ").last().unwrap();
+        let num: usize = split[0..split.len() - 1].parse()?;
+        page_len = match num {
+            n if n % 25 == 0 => num / 25,
+            n if n % 25 != 0 => num / 25 + 1,
+            _ => 0,
+        }
+    }
+    Ok(page_len)
 }
 
 pub async fn get_media_links(
-  url: &str,
-  base_url: &str,
+    url: &str,
+    base_url: &str,
 ) -> Result<Vec<String>, Box<dyn std::error::Error>> {
-  let mut links = Vec::new();
-  let dom = get_dom(url).await.unwrap();
+    let mut links = Vec::new();
+    let dom = get_dom(url).await?;
 
-  let items = match dom.clone() {
-      value if value.find(Class("post__attachment-link")).next() != None => {
-          dom.find(Class("post__attachment-link"))
-      }
-      value if value.find(Class("post__thumbnail")).next() != None => {
-          dom.find(Class("post__thumbnail"))
-      }
-      _ => dom.find(Class("post__content")),
-  };
+    let items = match dom.clone() {
+        value if value.find(Class("post__attachment-link")).next() != None => {
+            dom.find(Class("post__attachment-link"))
+        }
+        value if value.find(Class("post__thumbnail")).next() != None => {
+            dom.find(Class("post__thumbnail"))
+        }
+        _ => dom.find(Class("test")),
+    };
 
-  for item in items {
-      let print = match item {
-          value if value.attr("href") != None => value.attr("href"),
-          value if value.find(Name("img")).next().unwrap().attr("src") != None => {
-              value.find(Name("img")).next().unwrap().attr("src")
-          }
-          _ => Some("No"),
-      };
-      if print.unwrap() != "No" {
-          links.push([&base_url, print.unwrap()].join(""));
-      }
-  }
-  Ok(links)
+    for item in items {
+        let print = match item {
+            value if value.attr("href") != None => value.attr("href"),
+            value if value.find(Name("img")).next().unwrap().attr("src") != None => {
+                value.find(Name("img")).next().unwrap().attr("src")
+            }
+            _ => Some("No"),
+        };
+
+        if print.unwrap() != "No" {
+            links.push([&base_url, print.unwrap()].join(""));
+        }
+    }
+    Ok(links)
+}
+
+pub async fn download_img(url: &str, artist_name: &str, post_index: usize, image_index: usize) -> Result<(), Box<dyn std::error::Error>> {
+    let mut headers = HeaderMap::new();
+    headers.append(COOKIE, HeaderValue::from_str("__ddg2=6fryH34fRixR8HCV")?);
+
+    let client = reqwest::Client::new();
+    let resp = client.get(url).headers(headers).send().await?;
+    let image = resp.bytes().await?;
+    let reader = ImageReader::new(Cursor::new(image)).with_guessed_format()?;
+    assert_eq!(reader.format(), Some(ImageFormat::Jpeg));
+    let img = reader.decode()?;
+    Ok(img.save(format!("coomer/{}/Post {:0>3}/{:0>3}.jpg", artist_name, post_index+1, image_index+1))?)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,21 @@
+extern crate image;
 extern crate reqwest;
 extern crate select;
 extern crate tokio;
 
-mod helpers;
-use helpers::{get_page_num, gen_links, get_media_links};
+use http::{header::COOKIE, HeaderMap, HeaderValue};
+use image::io::Reader as ImageReader;
+use image::ImageFormat;
+use std::io::Cursor;
+use std::fs;
 
+mod helpers;
+use helpers::{download_img, gen_links, get_media_links, get_page_num};
 
 #[tokio::main]
 async fn main() {
+    // return download_video().await.unwrap();
+    // return download_img().await.unwrap();
     return artists().await.unwrap();
 }
 
@@ -25,28 +33,49 @@ async fn main() {
 //     Ok(())
 // }
 
+// async fn download_video() -> Result<(), Box<dyn std::error::Error>> {
+//     let video_url = "https://coomer.party/data/7e/83/7e831c7933356ba2cf8daa2491dd30cbe29803378f9f4613d9da2890441b4241.mp4?f=0gna42ojmzs2wp90ftg05_source.mp4";
+//     let mut headers = HeaderMap::new();
+//     headers.append(
+//         COOKIE,
+//         HeaderValue::from_str("__ddg2=6fryH34fRixR8HCV")?,
+//     );
+
+//     let client = reqwest::Client::new();
+//     let resp = client.get(video_url).headers(headers).send().await?;
+//     let video = resp.bytes().await?;
+//     let reader = ImageReader::new(Cursor::new(video))
+//     .with_guessed_format()?;
+//     // println!("{:?}", video);
+//     assert_eq!(reader.format(), Some(ImageFormat::Jpeg));
+//     let vid = reader.decode()?;
+//     Ok(())
+// }
+
 async fn artists() -> Result<(), Box<dyn std::error::Error>> {
     let base_url = "https://coomer.party";
     let url = "https://coomer.party/artists";
 
-    let site_page_len = get_page_num(&url).await.unwrap();
+    let site_page_len = get_page_num(&url).await?;
     for i in 0..site_page_len {
-        let site_links = gen_links(&url, "user-card__name", i, base_url)
-            .await
-            .unwrap();
+        let site_links = gen_links(&url, "user-card__name", i, base_url).await?;
 
         for link in site_links.iter() {
-            let artist_page_len = get_page_num(&link).await.unwrap();
+            let artist_page_len = get_page_num(&link).await?;
+            let artist_name = link.split("/").last().unwrap();
 
             for x in 0..artist_page_len {
-                let posts_links = gen_links(&link, "post-card__link", x, base_url)
-                    .await
-                    .unwrap();
+                let posts_links = gen_links(&link, "post-card__link", x, base_url).await?;
 
-                for li in posts_links.iter() {
-                    let media_links = get_media_links(li, base_url).await.unwrap();
+                for (post_index,li) in posts_links.iter().enumerate() {
+                    let media_links = get_media_links(li, base_url).await?;
 
-                    println!("{:?}", media_links);
+                    for (img_index, img) in media_links.iter().enumerate() {
+                        fs::create_dir_all(format!("coomer/{}/Post {}", artist_name, post_index+1))?;
+                        if img.split(".").last().unwrap() == "jpg" {
+                            download_img(img, artist_name, post_index+1, img_index).await;
+                        }
+                    }
                 }
             }
         }
